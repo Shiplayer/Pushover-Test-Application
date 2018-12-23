986adcff4841ea21cc9e05e338ab57a951fced97
Shiplayer <shiplayer@yandex.ru>
2018-12-23 14:36:17 +0300

    test work with api, send simple message

--- app/src/main/java/com/developer/ship/pushovertestapplication/MainActivity.kt
+++ app/src/main/java/com/developer/ship/pushovertestapplication/MainActivity.kt
@@ -2,11 +2,26 @@ package com.developer.ship.pushovertestapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            Observable.just("ur7zko984z7b6qkrei2ct5cyjne9j8")
+                .subscribeOn(Schedulers.io())
+                .subscribe { userToken ->
+                    PushoverAPI.instance.sendMessage(userToken = userToken, message = "hello").subscribe({response ->
+                        Log.i("MainActivity", response.body().toString())
+                    }, {error ->
+                        error.printStackTrace()
+                    })
+                }
+        }
     }
 }
---
+++ app/src/main/java/com/developer/ship/pushovertestapplication/PushoverAPI.kt
@@ -0,0 +1,38 @@
+package com.developer.ship.pushovertestapplication
+
+import com.google.gson.JsonObject
+import io.reactivex.Single
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+/**
+ * Created by Shiplayer on 23.12.18.
+ */
+interface PushoverAPI {
+    companion object {
+        val tokenApi = "a96cxd1qxafaztwhq2hwbagkf2hb94"
+        val baseURL = "https://api.pushover.net/"
+        public val instance by lazy{
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+
+            retrofit.create(PushoverAPI::class.java)
+        }
+    }
+
+    @FormUrlEncoded
+    @POST("1/messages.json")
+    fun sendMessage(
+            @Field("token") token:String = tokenApi,
+            @Field("user") userToken:String,
+            @Field("message") message:String
+    ) : Single<Response<JsonObject>>
+}
